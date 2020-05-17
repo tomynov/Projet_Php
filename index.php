f8df124e90128f3451d74a74fe7bda9998f1bd97
Tom <tom.cousdikian@ynov.com>
2020-05-17 22:15:44 +0200

    base_donnée_propre

--- index.php
+++ index.php
@@ -25,51 +25,48 @@
 
 <body id="page-top">
 
-  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-    <a class="navbar-brand js-scroll-trigger" href="#page-top">
-      <span class="d-block d-lg-none">Clarence Taylor</span>
-      <span class="d-none d-lg-block">
-        <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile.jpg" alt="">
-      </span>
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#about">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#experience">Experience</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#education">Education</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#skills">Skills</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#awards">Awards</a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#admin">Admin</a>
-        </li> -->
-        <li class="nav-item">
-          <a class="nav-link js-scroll-trigger" href="#compte">Compte</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <?php include("inc/header.inc.php"); ?>
 
   <div class="container-fluid p-0">
 
     <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="about">
       <div class="w-100">
-        <h1 class="mb-0">Tom
+        <?php
+          $pdo = new PDO("mysql:host=localhost;dbname=php_projet","root","", array
+          (PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+          $cv = $pdo->query("SELECT * FROM about");
+          while ($about = $cv->fetch(PDO::FETCH_OBJ))
+          {
+        ?>
+          
+          <h1 class="mb-0">
+          <span class="text-primary"><?php echo $about->title . "<br>"; ?></span>
+        </h1>
+        <div class="subheading mb-5"><?php echo $about->info . "<br>"; ?>
+          <a href="mailto:name@email.com">tom.cousdikian@ynov.com</a>
+        </div>
+        <p class="lead mb-5"><?php echo $about->texte . "<br>"; ?></p>
+        <div class="social-icons">
+          <a href="#">
+            <i class="fab fa-linkedin-in"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-github"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="#">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+
+        <?php
+            echo "<br>";
+          }
+        ?>
+
+        <!-- <h1 class="mb-0">Tom
           <span class="text-primary">COUSDIKIAN</span>
         </h1>
         <div class="subheading mb-5">3542 Berry Street · Cheyenne Wells, CO 80810 · (317) 585-8468 ·
@@ -89,7 +86,7 @@
           <a href="#">
             <i class="fab fa-facebook-f"></i>
           </a>
-        </div>
+        </div> -->
       </div>
     </section>
 
@@ -106,20 +103,26 @@
           while ($experiences = $result->fetch(PDO::FETCH_OBJ))
           {
           ?>
-           <h3 class="mb-0"> <?php echo $experiences->title . "<br>"; ?></h3>
-           <?php
-              echo $experiences->texte . "<br>";
-            ?>
-              <div class="resume-date text-md-right">
-                <span class="text-primary"><?php echo $experiences->date_redaction . "<br>"; ?> </span>
-              </div>
+
+          <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+            <div class="resume-content">
+              <h3 class="mb-0"><?php echo $experiences->title . "<br>"; ?></h3>
+              <p><?php echo $experiences->texte . "<br>"; ?></p>
+            </div>
+            <div class="resume-date text-md-right">
+              <span class="text-primary"><?php echo $experiences->date_redaction . "<br>"; ?></span>
+            </div>
+          </div>
+
           <?php
               echo "<br>";
           }
           ?>
+
+          
         
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <!-- <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="resume-content">
             <h3 class="mb-0">Senior Web Developer</h3>
             <div class="subheading mb-3">Intelitec Solutions</div>
@@ -128,7 +131,7 @@
           <div class="resume-date text-md-right">
             <span class="text-primary">March 2013 - Present</span>
           </div>
-        </div>
+        </div> -->
 
         <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="resume-content">
@@ -141,7 +144,7 @@
           </div>
         </div>
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+        <!-- <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
           <div class="resume-content">
             <h3 class="mb-0">Junior Web Designer</h3>
             <div class="subheading mb-3">Shout! Media Productions</div>
@@ -150,9 +153,9 @@
           <div class="resume-date text-md-right">
             <span class="text-primary">July 2010 - December 2011</span>
           </div>
-        </div>
+        </div> -->
 
-        <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
+        <!-- <div class="resume-item d-flex flex-column flex-md-row justify-content-between">
           <div class="resume-content">
             <h3 class="mb-0">Web Design Intern</h3>
             <div class="subheading mb-3">Shout! Media Productions</div>
@@ -161,7 +164,7 @@
           <div class="resume-date text-md-right">
             <span class="text-primary">September 2008 - June 2010</span>
           </div>
-        </div>
+        </div> -->
 
       </div>
 
